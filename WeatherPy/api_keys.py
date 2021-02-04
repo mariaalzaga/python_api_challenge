328bce30500ce457c5e5416c2473cfe86bdb54a0
mariaalzaga <m.alzaga96@gmail.com>
2021-02-04 21:28:22 +0000

    WeatherPy

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,4 +1,8 @@
 # OpenWeatherMap API Key
-api_key = "2f430d7e718eebfb75723945b3a94d3d"
+weather_api_key = "a6a71f9604b1a38f6deb7dbe79c0c329"
 
+# Google API Key
+g_key = "AIzaSyAPBo6bo2SNOixytND3jmsC8lsqz2Hnv7s"
 
+# Mapbox Token
+m_key = "pk.eyJ1IjoiaWNvZGUxMyIsImEiOiJja2Zra2lva2owdHo0MnptanV3bnNlZGprIn0.zfeW_ah_9YVqEk3UpQE_6Q"
