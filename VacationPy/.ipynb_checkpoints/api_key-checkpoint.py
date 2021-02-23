69ba64234619902e4bc42dd9202d556ab1d9e3cc
mariaalzaga <m.alzaga96@gmail.com>
2021-02-23 19:58:51 +0000

    vaction

--- VacationPy/.ipynb_checkpoints/api_key-checkpoint.py
+++ VacationPy/.ipynb_checkpoints/api_key-checkpoint.py
@@ -7,4 +7,5 @@ g_key = "AIzaSyDpJzyz14Iohr2vZDynBh_h-qn6OvrxDbM"
 # Mapbox Token
 m_key = "pk.eyJ1IjoiaWNvZGUxMyIsImEiOiJja2Zra2lva2owdHo0MnptanV3bnNlZGprIn0.zfeW_ah_9YVqEk3UpQE_6Q"
 
+
 gmaps.configure(api_key=os.environ["AIzaSyDpJzyz14Iohr2vZDynBh_h-qn6OvrxDbM"])
--- VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++
@@ -1,4 +0,0 @@
-# OpenWeatherMap API Key
-api_key = "2f430d7e718eebfb75723945b3a94d3d"
-
-
